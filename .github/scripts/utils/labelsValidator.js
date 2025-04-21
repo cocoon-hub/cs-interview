0b1b91088aa1bb33ec3835e91296cbeaee3fab6b
simhani1 <simhani1@gmail.com>
2025-04-21 21:27:19 +0900

    refactor: label validation script to use async run function

--- .github/scripts/utils/labelsValidator.js
+++ .github/scripts/utils/labelsValidator.js
@@ -1,14 +1,26 @@
-const issue = context.payload.issue;
-const labels = issue.labels || [];
+import { context, getOctokit } from '@actions/github';
+import * as core from '@actions/core';
 
-if (labels.length !== 0) {
-  console.log('label을 하나 이상 추가해야만 합니다.');
+async function run() {
+  const token = process.env.GITHUB_TOKEN;
+  const issue = context.payload.issue;
+  const labels = issue.labels || [];
 
-  await github.rest.issues.update({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    issue_number: issue.number,
-    state: 'open',
-  });
-  throw new Error('라벨 없음 - 이슈 재오픈 후 워크플로우 종료');
+  if (labels.length === 0) {
+    console.log('라벨이 없습니다. 이슈를 reopen합니다.');
+
+    const octokit = getOctokit(token);
+    await octokit.rest.issues.update({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: issue.number,
+      state: 'open',
+    });
+
+    core.setFailed('라벨 없음 - 이슈 재오픈');
+  } else {
+    console.log('라벨 있음 - 다음 단계로 진행');
+  }
 }
+
+run();
